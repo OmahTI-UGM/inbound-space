78a60bddb24fd6daf5fea48f351ee2190912d3e5
stackoverprof <mail.errbint@gmail.com>
2020-10-07 23:28:04 +0700

    story utama page for mobile done

---
+++ components/mobile/Story.js
@@ -0,0 +1,154 @@
+import React from 'react'
+import Styled from '@emotion/styled'
+    
+const Index = ({pageId, content, prevpage, nextpage}) => {
+    return (
+        <Wrapper pageId={pageId}>
+            <div className="content">
+                <div className="img">
+                    <img src={`/img/story/${pageId}.svg`} alt=""/>
+                </div>
+                <div className="textbox">
+                    <p className="p-content">{content[pageId].words}</p>
+                    <hr/>
+                    <div className="btns">
+                        <button onClick={()=> pageId >= 2? prevpage():''}className="prev"><svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.03364 7.36694L11.3275 1.83237C12.6607 1.03847 14.3508 1.99912 14.3508 3.55076L14.3508 14.6199C14.3508 16.1715 12.6607 17.1322 11.3275 16.3383L2.03363 10.8037C0.731378 10.0282 0.731377 8.14244 2.03364 7.36694Z" stroke="white" stroke-width="2"/></svg></button>
+                        <p>{pageId}/5</p>
+                        <button onClick={()=> nextpage()}className="next"><svg width="24" height="28" viewBox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.5905 12.1559C23.8927 12.9314 23.8927 14.8171 22.5905 15.5926L3.39233 27.0253C2.05918 27.8192 0.369021 26.8585 0.369021 25.3069L0.369022 2.44164C0.369022 0.890004 2.05918 -0.0706496 3.39233 0.723249L22.5905 12.1559Z" fill="#D7D7DA"/></svg></button>
+                    </div>
+                </div>
+                <h1>.story</h1>
+            </div>
+        </Wrapper>
+    );
+}
+    
+const Wrapper = Styled.div(({pageId}) =>`
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    background-image: url('/img/story/m-bg.svg');
+    background-position: right;
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    div.content{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
+        padding: 0 32px;
+    }
+    
+    div.img{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 32px;
+
+        img{
+            height: 300px;
+        }
+    }
+    .textbox{
+        width: 100%;
+        min-width: 350px;
+        max-width: 500px;
+        height: 208px;
+
+        background: #050216;
+        border: 1px solid #FFFFFF;
+        box-sizing: border-box;
+        box-shadow: -4px -4px 4px rgba(255, 255, 255, 0.1), 4px 4px 4px #000000;
+        border-radius: 16px;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-direction: column;
+
+        padding: 22px;
+        padding-top: 28px;
+
+        .p-content{
+            min-height: 92px;
+            font-family: 'Exo2-med';
+            font-style: normal;
+            font-size: 19px;
+            line-height: 23px;
+            text-align: center;
+            margin: 0;
+            margin-bottom: 24px;
+
+            color: #FFFFFF;
+        }
+
+        hr{
+            border: 1px solid #FFFFFF;
+            margin: 0 0;
+            width: 100%;
+        }
+    }
+    h1{
+        font-family: 'Poppkorn';
+        font-style: normal;
+        font-weight: normal;
+        font-size: 75px;
+        line-height: 75px;
+        margin: 0;
+        background: -webkit-linear-gradient(0deg, #F29C1F 40%, #EA1984 130%);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        display: inline-block;
+        margin-top: 12px;
+    }
+
+    .btns{
+        margin-top: 12px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+
+        button{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        p{
+            margin: 0;
+            font-family: 'Exo2-med';
+            font-style: normal;
+            font-weight: 800;
+            font-size: 19px;
+            line-height: 23px;
+            text-align: center;
+
+            color: #4D35D5;
+        }
+    }
+    button.prev{
+        width: 44px;
+        height: 34px;
+    
+        background: none;
+        border: 2px solid #FFFFFF;
+        box-sizing: border-box;
+        border-radius: 8px;
+        filter: opacity(${pageId < 2 ? 0 : 1});
+    
+        &:focus{
+            outline: none;
+        }
+    }
+    button.next{
+        background: none;
+        border: none;
+    }
+`)
+    
+export default Index
--- pages/story/index.js
+++ pages/story/index.js
@@ -1,10 +1,23 @@
-import React,{ useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Styled from '@emotion/styled'
 import dataStory from '../../sample-api/story.json'
+import Mobile from '../../components/mobile/Story'
     
 const Index = () => {
     const [pageId, setpageId] = useState(1)
     const [imgOpac, setimgOpac] = useState(1);
+    const [screen, setScreen] = useState(undefined);
+
+    useEffect(() => {
+        setScreen(window.innerWidth);
+
+        window.addEventListener('resize', handleWindowSizeChange);
+    });
+
+    const handleWindowSizeChange = () => {
+        setScreen(window.innerWidth);
+    };
+
     const content = dataStory.pages
 
     function nextpage(){
@@ -27,35 +40,47 @@ const Index = () => {
         }, 250);
     }
 
-    return (
-        <Wrapper pageId={pageId} imgOpac={imgOpac}>
-            <div className="containslide">
-                <div className="slidleft"></div>
-                <div className="img">
-                    <img src={`/img/story/${pageId}.svg`} alt=""/>
-                </div>
-                <div className="slidright"></div>
-            </div>
-            <div className="containslide">
-                <div className="slidright"></div>
-                <div className="whitecontent">
-                    <p>{content[pageId].words}</p>
-                    <div className="pagebtn">
-                        <button onClick={()=> prevpage()}className="prev"><svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.75001 16.1651C1.08333 15.2028 1.08334 12.7972 2.75 11.8349L19.25 2.30866C20.9167 1.34641 23 2.54922 23 4.47372L23 23.5263C23 25.4508 20.9167 26.6536 19.25 25.6913L2.75001 16.1651Z" stroke="white" stroke-width="3"/></svg></button>
-                    
-                        <button onClick={()=> nextpage()}className="next"><p>NEXT</p><svg width="27" height="30" viewBox="0 0 27 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M26 13.2679C27.3333 14.0377 27.3333 15.9622 26 16.732L3.5 29.7224C2.16667 30.4922 0.499999 29.53 0.499999 27.9904L0.5 2.00962C0.5 0.470019 2.16667 -0.492234 3.5 0.277567L26 13.2679Z" fill="#180F4A"/></svg></button>
-                        
+    if(screen > 1080){
+        return (
+            <Wrapper pageId={pageId} imgOpac={imgOpac} screen={screen}>
+                <div className="containslide">
+                    <div className="slidleft"></div>
+                    <div className="img">
+                        <img src={`/img/story/${pageId}.svg`} alt=""/>
                     </div>
+                    <div className="slidright"></div>
                 </div>
-                <div className="slidleft"></div>
-            </div>
-            
-            <h1>.story</h1>
-            <div className="mvgbg">
-                <div className="bg"></div>
-            </div>
-        </Wrapper>
-    );
+                <div className="containslide">
+                    <div className="slidright"></div>
+                    <div className="whitecontent">
+                        <p>{content[pageId].words}</p>
+                        <div className="pagebtn">
+                            <button onClick={()=> prevpage()}className="prev"><svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.75001 16.1651C1.08333 15.2028 1.08334 12.7972 2.75 11.8349L19.25 2.30866C20.9167 1.34641 23 2.54922 23 4.47372L23 23.5263C23 25.4508 20.9167 26.6536 19.25 25.6913L2.75001 16.1651Z" stroke="white" stroke-width="3"/></svg></button>
+                            <button onClick={()=> nextpage()}className="next"><p>NEXT</p><svg width="27" height="30" viewBox="0 0 27 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M26 13.2679C27.3333 14.0377 27.3333 15.9622 26 16.732L3.5 29.7224C2.16667 30.4922 0.499999 29.53 0.499999 27.9904L0.5 2.00962C0.5 0.470019 2.16667 -0.492234 3.5 0.277567L26 13.2679Z" fill="#180F4A"/></svg></button>
+                        </div>
+                    </div>
+                    <div className="slidleft"></div>
+                </div>
+                
+                <h1>.story</h1>
+                <div className="mvgbg">
+                    <div className="bg"></div>
+                </div>
+
+                <div className="imgloader">
+                    <img src={`/img/story/1.svg`} alt=""/>
+                    <img src={`/img/story/2.svg`} alt=""/>
+                    <img src={`/img/story/3.svg`} alt=""/>
+                    <img src={`/img/story/4.svg`} alt=""/>
+                    <img src={`/img/story/5.svg`} alt=""/>
+                </div>
+            </Wrapper>
+        )
+    }else{
+        return(
+            <Mobile pageId={pageId} content={content} prevpage={prevpage} nextpage={nextpage}/>
+        )
+    }
 }
     
 const Wrapper = Styled.div(({pageId, imgOpac}) =>`
@@ -196,6 +221,12 @@ p{
     font-size: 32px;
     margin: 0;
     max-width: 480px;
+}
+.imgloader{
+    position: fixed;
+    filter: opacity(0);
+    transform: scale(0.01);
+    z-index: -100;
 }
 `)
     

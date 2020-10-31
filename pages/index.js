6050939a9053647427eae431237d6347145a76f7
stackoverprof <mail.errbint@gmail.com>
2020-10-31 21:45:08 +0700

    minor fix

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Index = () => {
       
     // To calculate the no. of days between two dates 
     let dayDiffer = timeDiffer / (1000 * 3600 * 24); 
-    setdate(Math.trunc(dayDiffer)+1)
+    setdate(Math.trunc(dayDiffer))
     
     const interval = setInterval(() => {
       setred(!red)
